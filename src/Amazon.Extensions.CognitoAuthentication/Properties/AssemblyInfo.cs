682c8c20852aabd579521885a15436c8022edce1
Hamza Assyad <assyad@outlook.com>
2018-10-09 10:43:28 -0700

    Renaming the assembly, changing the target framework to netstandard20, removing useless gitignore section and fixing a typo. (#1)
    * Archive of the developer preview of AWSSDK.Extensions.CognitoAuthentication
    
    * Project renaming, removing CoreCLR/NET45 specific csproj and moving toward netstandard20
    
    * Changing the default assembly name and fixing a classname typo
    
    * PR #1 Feedback, Copyright date set to 2018 and removal of unused csproj fields
    
    * Updated nuspec file

---
+++ src/Amazon.Extensions.CognitoAuthentication/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AWSSDK.Extensions.CognitoAuthentication")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Amazon.com, Inc")]
+[assembly: AssemblyProduct("Amazon Web Services SDK for .NET extensions for Cognito Authentication")]
+[assembly: AssemblyDescription("Amazon Web Services SDK for .NET extensions for Cognito Authentication")]
+[assembly: AssemblyCopyright("Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+
+[assembly: AssemblyVersion("1.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
