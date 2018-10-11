ff94fcf747b687c3cf934be9169e091418bf50b9
Hamza Assyad <hamzaad@amazon.com>
2018-10-11 11:23:45 -0700

    Refactoring a try catch block and setting the Status to immutable

--- src/Amazon.Extensions.CognitoAuthentication/Util/CognitoConstants.cs
+++ src/Amazon.Extensions.CognitoAuthentication/Util/CognitoConstants.cs
@@ -18,50 +18,50 @@ namespace Amazon.Extensions.CognitoAuthentication
     public class CognitoConstants
     {
         // Challenge Parameters
-        public static string ChlgParamSrpA = "SRP_A";
-        public static string ChlgParamSrpB = "SRP_B";
-        public static string ChlgParamSecretHash = "SECRET_HASH";
-        public static string ChlgParamUsername = "USERNAME";
-        public static string ChlgParamChallengeName = "CHALLENGE_NAME";
-        public static string ChlgParamSalt = "SALT";
-        public static string ChlgParamSecretBlock = "SECRET_BLOCK";
-        public static string ChlgParamUserIDSrp = "USER_ID_FOR_SRP";
-        public static string ChlgParamRefreshToken = "REFRESH_TOKEN";
+        public const string ChlgParamSrpA = "SRP_A";
+        public const string ChlgParamSrpB = "SRP_B";
+        public const string ChlgParamSecretHash = "SECRET_HASH";
+        public const string ChlgParamUsername = "USERNAME";
+        public const string ChlgParamChallengeName = "CHALLENGE_NAME";
+        public const string ChlgParamSalt = "SALT";
+        public const string ChlgParamSecretBlock = "SECRET_BLOCK";
+        public const string ChlgParamUserIDSrp = "USER_ID_FOR_SRP";
+        public const string ChlgParamRefreshToken = "REFRESH_TOKEN";
 
-        public static string ChlgParamPassSecretBlock = "PASSWORD_CLAIM_SECRET_BLOCK";
-        public static string ChlgParamPassSignature = "PASSWORD_CLAIM_SIGNATURE";
-        public static string ChlgParamTimestamp = "TIMESTAMP";
-        public static string ChlgParamDeliveryDest = "CODE_DELIVERY_DESTINATION";
-        public static string ChlgParamDeliveryMed = "CODE_DELIVERY_DELIVERY_MEDIUM";
+        public const string ChlgParamPassSecretBlock = "PASSWORD_CLAIM_SECRET_BLOCK";
+        public const string ChlgParamPassSignature = "PASSWORD_CLAIM_SIGNATURE";
+        public const string ChlgParamTimestamp = "TIMESTAMP";
+        public const string ChlgParamDeliveryDest = "CODE_DELIVERY_DESTINATION";
+        public const string ChlgParamDeliveryMed = "CODE_DELIVERY_DELIVERY_MEDIUM";
 
-        public static string ChlgParamSmsMfaCode = "SMS_MFA_CODE";
-        public static string ChlgParamDeviceKey = "DEVICE_KEY";
+        public const string ChlgParamSmsMfaCode = "SMS_MFA_CODE";
+        public const string ChlgParamDeviceKey = "DEVICE_KEY";
 
-        public static string ChlgParamUserAttrs = "userAttributes";
-        public static string ChlgParamRequiredAttrs = "requiredAttributes";
-        public static string ChlgParamUserAttrPrefix = "userAttributes.";
-        public static string ChlgParamNewPassword = "NEW_PASSWORD";
-        public static string ChlgParamPassword = "PASSWORD";
+        public const string ChlgParamUserAttrs = "userAttributes";
+        public const string ChlgParamRequiredAttrs = "requiredAttributes";
+        public const string ChlgParamUserAttrPrefix = "userAttributes.";
+        public const string ChlgParamNewPassword = "NEW_PASSWORD";
+        public const string ChlgParamPassword = "PASSWORD";
 
         // User Attributes
-        public static string UserAttrEmail = "email";
-        public static string UserAttrPhoneNumber = "phone_number";
+        public const string UserAttrEmail = "email";
+        public const string UserAttrPhoneNumber = "phone_number";
 
         // Device Attributes
-        public static string DeviceAttrName = "device_name";
-        public static string DeviceAttrRemembered = "remembered";
-        public static string DeviceAttrNotRemembered = "not_remembered";
+        public const string DeviceAttrName = "device_name";
+        public const string DeviceAttrRemembered = "remembered";
+        public const string DeviceAttrNotRemembered = "not_remembered";
 
-        public static string DeviceChlgParamSalt = "salt";
-        public static string DeviceChlgParamVerifier = "verifier";
+        public const string DeviceChlgParamSalt = "salt";
+        public const string DeviceChlgParamVerifier = "verifier";
 
         // User status
-        public static string StatusUnconfirmed = "UNCONFIRMED";
-        public static string StatusConfirmed = "CONFIRMED";
-        public static string StatusArchivedd = "ARCHIVED";
-        public static string StatusCompromised = "COMPROMISED";
-        public static string StatusUnknown = "UNKNOWN";
-        public static string StatusResetRequired = "RESET_REQUIRED";
-        public static string StatusForceChangePassword = "FORCE_CHANGE_PASSWORD";
+        public const string StatusUnconfirmed = "UNCONFIRMED";
+        public const string StatusConfirmed = "CONFIRMED";
+        public const string StatusArchivedd = "ARCHIVED";
+        public const string StatusCompromised = "COMPROMISED";
+        public const string StatusUnknown = "UNKNOWN";
+        public const string StatusResetRequired = "RESET_REQUIRED";
+        public const string StatusForceChangePassword = "FORCE_CHANGE_PASSWORD";
     }
 }
