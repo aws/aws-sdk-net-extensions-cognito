4ba58d65773fa2918150e6cd4f4bf633580fae45
Hamza Assyad <hamzaad@amazon.com>
2018-12-14 15:15:52 -0800

    Switching AssemblyVersion to 1.0 before GA

--- src/Amazon.Extensions.CognitoAuthentication/Properties/AssemblyInfo.cs
+++ src/Amazon.Extensions.CognitoAuthentication/Properties/AssemblyInfo.cs
@@ -15,5 +15,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 
-[assembly: AssemblyVersion("0.9")]
+[assembly: AssemblyVersion("1.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
