68847a96220aac04e0e4769d601e6150938a0a4b
Graham Dodd <graham.dodd@bhnetwork,com>
2020-09-10 20:47:41 +0100

    Add support for TOTP challenges when responding to two factor requests

--- src/Amazon.Extensions.CognitoAuthentication/CognitoAuthenticationClasses.cs
+++ src/Amazon.Extensions.CognitoAuthentication/CognitoAuthenticationClasses.cs
@@ -111,19 +111,35 @@ namespace Amazon.Extensions.CognitoAuthentication
     }
 
     /// <summary>
-    /// Class containing the necessary properities to respond to an SMS MFA authentication challenge
+    /// Class containing the necessary properities to respond to an MFA authentication challenge
     /// </summary>
-    public class RespondToSmsMfaRequest
+    public class RespondToMfaRequest
     {
         /// <summary>
         /// The session ID for the current authentication flow.
         /// </summary>
-        public string SessionID { get; set; }
+        public virtual string SessionID { get; set; }
 
         /// <summary>
         /// The MFA verification code needed to authenticate the user.
         /// </summary>
-        public string MfaCode { get; set; }
+        public virtual string MfaCode { get; set; }
+
+        /// <summary>
+        /// The MFA verification code needed to authenticate the user.
+        /// </summary>
+        public virtual ChallengeNameType ChallengeType { get; set; }        
+    }
+
+    /// <summary>
+    /// Class containing the necessary properities to respond to an MFA authentication challenge
+    /// </summary>
+    public class RespondToSmsMfaRequest : RespondToMfaRequest
+    {
+        /// <summary>
+        /// The MFA verification code needed to authenticate the user.
+        /// </summary>
+        public virtual ChallengeNameType ChallengeType { get { return ChallengeNameType.SMS_MFA; } set { } }
     }
 
     /// <summary>
--- src/Amazon.Extensions.CognitoAuthentication/CognitoUserAuthentication.cs
+++ src/Amazon.Extensions.CognitoAuthentication/CognitoUserAuthentication.cs
@@ -139,16 +139,29 @@ namespace Amazon.Extensions.CognitoAuthentication
         /// if one exists</returns>
         public async Task<AuthFlowResponse> RespondToSmsMfaAuthAsync(RespondToSmsMfaRequest smsMfaRequest)
         {
+            return await RespondToMfaAuthAsync(smsMfaRequest).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Uses the properties of the RespondToSmsMfaRequest object to respond to the current MFA 
+        /// authentication challenge using an asynchronous call
+        /// </summary>
+        /// <param name="smsMfaRequest">RespondToSmsMfaRequest object containing the necessary parameters to
+        /// respond to the current MFA authentication challenge</param>
+        /// <returns>Returns the AuthFlowResponse object that can be used to respond to the next challenge, 
+        /// if one exists</returns>
+        public async Task<AuthFlowResponse> RespondToMfaAuthAsync(RespondToMfaRequest mfaRequest)
+        {
             RespondToAuthChallengeRequest challengeRequest = new RespondToAuthChallengeRequest
             {
                 ChallengeResponses = new Dictionary<string, string>
                     {
-                        { CognitoConstants.ChlgParamSmsMfaCode, smsMfaRequest.MfaCode},
+                        { ChallengeParamCodeType(mfaRequest.ChallengeType), mfaRequest.MfaCode},
                         { CognitoConstants.ChlgParamUsername, Username }
                     },
-                Session = smsMfaRequest.SessionID,
+                Session = mfaRequest.SessionID,
                 ClientId = ClientID,
-                ChallengeName = ChallengeNameType.SMS_MFA
+                ChallengeName = mfaRequest.ChallengeType
             };
 
             if (!string.IsNullOrEmpty(SecretHash))
@@ -166,6 +179,14 @@ namespace Amazon.Extensions.CognitoAuthentication
                 challengeResponse.ChallengeName,
                 challengeResponse.ChallengeParameters,
                 new Dictionary<string, string>(challengeResponse.ResponseMetadata.Metadata));
+        }    
+        
+        private string ChallengeParamCodeType(ChallengeNameType challengeType)
+        {
+            if (challengeType == ChallengeNameType.SMS_MFA) return CognitoConstants.ChlgParamSmsMfaCode;
+            if (challengeType == ChallengeNameType.SOFTWARE_TOKEN_MFA) return CognitoConstants.ChlgParamSoftwareTokenMfaCode;
+            
+            return null;
         }
 
         /// <summary>
