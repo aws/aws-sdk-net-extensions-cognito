f317e256a43371ec82f16581e7d593862b00557c
Phil Asmar <phil.asmar@gmail.com>
2024-10-15 22:14:02 -0400

    feat: add support for .NET SDK V4

--- src/Amazon.Extensions.CognitoAuthentication/Properties/AssemblyInfo.cs
+++ src/Amazon.Extensions.CognitoAuthentication/Properties/AssemblyInfo.cs
@@ -1,19 +1,6 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("AWSSDK.Extensions.CognitoAuthentication")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Amazon.com, Inc")]
-[assembly: AssemblyProduct("Amazon Web Services SDK for .NET extensions for Cognito Authentication")]
-[assembly: AssemblyDescription("Amazon Web Services SDK for .NET extensions for Cognito Authentication")]
-[assembly: AssemblyCopyright("Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.")]
-[assembly: AssemblyTrademark("")]
+﻿using System.Runtime.InteropServices;
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-
-[assembly: AssemblyVersion("1.0")]
-[assembly: AssemblyFileVersion("2.5.3.0")]
